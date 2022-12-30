f0f61ac060fa69fc16c4dd27c4ac45688a5df064
Andre <aspbarruda@gmail.com>
2022-12-29 23:56:58 -0300

    Shows right answer, asks if want to replay

--- hangman.rb
+++ hangman.rb
@@ -5,7 +5,7 @@ def initial_printing(answer, used_letters, guesses)
   print "Used Letters: "
   used_letters.each {|value| print "#{value} "}
   puts "\n\n"
-  puts "Guess #{guesses} out of 7:\n"
+  puts "Guess #{guesses} out of #{MAX_GUESSES}:\n"
 end
 
 def make_a_guess()
@@ -21,12 +21,14 @@ def make_a_guess()
 end
 
 def compare(guess, secret_word, answer)
+  temp = []
+  answer.each_index {|i| temp[i] = answer[i]}
   i = 0
   secret_word.each_char { |letter|
-    answer[i] = letter if letter == guess
+    temp[i] = letter if letter == guess
     i += 1
   }
-  answer
+  temp
 end
 
 def check_validity(letter)
@@ -36,6 +38,8 @@ end
 
 file = File.read "google-10000-english-no-swears.txt"
 file = file.split("\n")
+
+MAX_GUESSES = 7
 
 possible_words = file.select {|value| value if value.length > 4 && value.length < 13}
 
@@ -47,9 +51,23 @@ while game_running
   used_letters = []
   secret_word = possible_words[rand(possible_words.length)]
   answer = Array.new(secret_word.length, "_")
-  until have_won
+  until have_won || guess_number > MAX_GUESSES
     initial_printing(answer, used_letters, guess_number)
     guess = make_a_guess()
-    answer = compare(guess, secret_word, answer)
+    temp = compare(guess, secret_word, answer)
+    used_letters.push guess
+    unless temp.include? "_"
+      have_won = true
+      next
+    end
+    guess_number += 1 if temp == answer
+    answer.each_index {|i| answer[i] = temp[i]}
   end
+  if have_won
+    puts "You won! Type 'y' to play again or any other key to quit"
+  else
+    puts "You lost, the correct answer was #{secret_word}! Type 'y' to play again or any other key to quit"
+  end
+  choice = gets.chomp
+  game_running = false if choice.upcase != "Y"
 end
