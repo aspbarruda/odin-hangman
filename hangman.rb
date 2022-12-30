480d2a2fc91a48aced5ac9ff1721a1e956694125
Andre <aspbarruda@gmail.com>
2022-12-29 23:19:26 -0300

    Checks the guess and updates "Answer" variable

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,39 @@
+def initial_printing(answer, used_letters, guesses)
+  print "Answer: "
+  answer.each {|value| print "#{value} "}
+  puts "\n\n"
+  print "Used Letters: "
+  used_letters.each {|value| print "#{value} "}
+  puts "\n\n"
+  puts "Guess #{guesses} out of 7:\n"
+end
+
+def make_a_guess()
+  while true
+    guess = gets.chomp
+    unless check_validity(guess)
+      puts "Enter a single letter only:"
+      next
+    end
+    break
+  end
+  guess
+end
+
+def compare(guess, secret_word, answer)
+  i = 0
+  secret_word.each_char { |letter|
+    answer[i] = letter if letter == guess
+    i += 1
+  }
+  answer
+end
+
+def check_validity(letter)
+  return true unless letter.length != 1 || letter.match?(/[^a-z^A-Z]/)
+  false
+end
+
 file = File.read "google-10000-english-no-swears.txt"
 file = file.split("\n")
 
@@ -7,18 +43,13 @@ game_running = true
 
 while game_running
   have_won = false
-  guesses = 1
+  guess_number = 1
   used_letters = []
   secret_word = possible_words[rand(possible_words.length)]
   answer = Array.new(secret_word.length, "_")
   until have_won
-    print "Answer: "
-    answer.each {|value| print "#{value} "}
-    puts "\n\n"
-    print "Used Letters: "
-    used_letters.each {|value| print "#{value} "}
-    puts "\n\n"
-    puts "Guess #{guesses} out of 7:\n"
-    guess = gets.chomp
+    initial_printing(answer, used_letters, guess_number)
+    guess = make_a_guess()
+    answer = compare(guess, secret_word, answer)
   end
 end
